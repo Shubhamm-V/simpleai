07500423cf2ee4f28bf90edb3c1bc3edb6441709
Shubham Vyavhare <shubhamvyavhare30@gmail.com>
2023-06-21 14:46:24 +0530

    Fixed antd delay issue with SSR

---
+++ client/pages/_document.tsx
@@ -0,0 +1,58 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+import { doExtraStyle } from "../scripts/getAnddCss";
+
+import { StyleProvider, createCache } from "@ant-design/cssinjs";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const cache = createCache();
+
+    let fileName = "";
+
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          (
+            <StyleProvider cache={cache}>
+              <App {...props} />
+            </StyleProvider>
+          ),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    fileName = doExtraStyle({
+      cache,
+    });
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {fileName && <link rel="stylesheet" href={`/${fileName}`} />}
+        </>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="eng">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
